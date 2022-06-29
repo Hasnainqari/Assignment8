ea00d3d2338abe5d101c01fe34bafe719d953b73
Hasnain Qari <qarihasnain43@gmail.com>
2022-06-29 08:43:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment8
+ JavaScript Assignment
---
+++ app.js
@@ -0,0 +1,30 @@
+// Task  01 Head & Tail Game;
+
+// var userName = prompt("Enter Your name");
+// var headTale = prompt("What do you want to?\nH OR T");
+// var ht = ["H","T"];
+// var gameStart = Math.floor(Math.random() * ht.length);
+// var word = ht[gameStart];
+// if(headTale === word){
+//     alert(userName + " is Winner");
+// }else if (headTale === ""){
+//     alert("Enter What do you Want?");
+// }else if (headTale !== "H" || headTale == "F"){
+//     alert("Neither Head Nor Tail");
+// }else{
+//     alert(userName+" is loss")
+// }
+
+// Task  02 Dice;
+// var dicegame = [1, 2, 3, 4, 5, 6];
+// console.log(Math.floor(Math.random() * dicegame.length));
+
+// Task  03 Password Genrator;
+// var str = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm1234567890&%$#@!-+/"
+// var password = "";
+
+// for(var i = 0; i< 10; i++){
+//     var genrator = Math.floor(Math.random() * str.length);
+//     password += str[genrator];
+// }
+// console.log(password)
